055593f5528fb7bbb5061dd17b220b47f3b7c937
maher <maher.ahmed@engaz.ai>
2026-02-13 16:46:39 +0200

    feat: add loading components

---
+++ src/app/components/loading/loading.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading',
+  standalone: true,
+  templateUrl: './loading.html',
+  styleUrl: './loading.scss',
+})
+export class LoadingComponent {}
